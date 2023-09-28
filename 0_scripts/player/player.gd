2b84437cb5f8e4dc3df50919c7643135c8dee6a7
Jeff Hilton <jeffhilton.code@gmail.com>
2023-09-28 10:34:34 -0600

    move player scripts

--- 0_scripts/player/player.gd
+++ 0_scripts/player/player.gd
@@ -1,16 +1,14 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var animator = $animated_sprite_2d
+
 @export_group('Movement Settings')
 ## How fast the player moves (in .1 pixel/second units)
 @export var speed = 300.0
 
 ## Initial jump speed (in .1 pixel/second^2 units)
 @export var jump_force = -400.0
-
-@export_group('Components')
-## The player's animated sprite component.
-@export var animator: AnimatedSprite2D = null
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
