65a3dec903b309018190d9f524547fbae8d0ea38
Jeff Hilton <jeffhilton.code@gmail.com>
2023-09-27 19:43:30 -0600

    basic player character

---
+++ scripts/fsm/state.gd
@@ -0,0 +1,16 @@
+extends Node2D
+class_name State
+
+signal transitioned
+
+func on_process(delta: float):
+	pass
+
+func on_physics_process(delta: float):
+	pass
+
+func on_enter():
+	pass
+	
+func on_exit():
+	pass
