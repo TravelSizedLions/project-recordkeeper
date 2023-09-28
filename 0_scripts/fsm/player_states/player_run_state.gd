7aecb2cd3b4e020ae95f8ed97cfa5f262e56b44b
Jeff Hilton <jeffhilton.code@gmail.com>
2023-09-28 09:59:06 -0600

    jump flow feels about right

--- 0_scripts/fsm/player_states/player_run_state.gd
+++ 0_scripts/fsm/player_states/player_run_state.gd
@@ -1,22 +1,14 @@
 extends State
 class_name PlayerRunState
 
-var player: Player = null
-
-func __on_ready():
-	player = get_tree().get_first_node_in_group('player')
-
-func __on_process(_delta: float):
-	player.update_direction()
+@onready var player: Player = get_tree().get_first_node_in_group('player')
 
 func __on_physics_process(_delta: float):
-	var direction = Input.get_axis('left', 'right')
-	if direction:
-		player.velocity.x = direction * player.speed
-	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, player.speed)
-		if player.velocity.x < 0.2:
-			transitioner.emit(PlayerIdleState)
+	player.handle_run()
+	if not player.is_moving():
+		transitioner.emit(PlayerIdleState)
+	elif player.pressed_jump():
+		transitioner.emit(PlayerStartJumpState)
 
 func __on_enter():
 	player.animator.play('run')
