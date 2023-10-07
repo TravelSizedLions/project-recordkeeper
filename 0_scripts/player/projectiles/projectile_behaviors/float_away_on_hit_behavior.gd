01a13406fd0853890f046ccf121090e33f1e943b
Jeff Hilton <jeffhilton.code@gmail.com>
2023-10-06 23:11:46 -0600

    sketch of player health UI, unconnected

--- 0_scripts/player/projectiles/projectile_behaviors/float_away_on_hit_behavior.gd
+++ 0_scripts/player/projectiles/projectile_behaviors/float_away_on_hit_behavior.gd
@@ -2,6 +2,8 @@ extends ProjectileOnHitBehavior
 class_name FloatAwayOnHitBehavior
 
 func on_hit(body):
+	# TODO: If the projectile hits the floor, the body owner is the scene root, 
+	# so this will just find the first instance of CanFloatAway anywhere in the scene...
 	var attribute: CanFloatAway = NodeUtils.get_child(body.owner, CanFloatAway)
 	if attribute:
 		attribute.apply_attribute()
