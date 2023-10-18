d9bb9f7e72c80f479f0c3a185b847d42e82d67f8
Jeff Hilton <jeffhilton.code@gmail.com>
2023-10-17 22:38:43 -0600

    fixed music and boss spawning issues

--- 0_scripts/sound/music_with_intro.gd
+++ 0_scripts/sound/music_with_intro.gd
@@ -1,7 +1,21 @@
-extends AudioStreamPlayer
+extends Triggerable
 class_name MusicWithIntro
 
+@export var __start_automatically: bool = true
+
+@export var __intro: AudioStream
 @export var __main_loop: AudioStream
 
-func on_finished():
+func _ready():
+	if __start_automatically:
+		__start()
+	
+func _on_trigger():
+	__start()
+
+func __start():
+	SoundManager.stop_music()	
+	SoundManager.play_music(__intro).finished.connect(__on_finished)
+
+func __on_finished():
 	SoundManager.play_music(__main_loop)
