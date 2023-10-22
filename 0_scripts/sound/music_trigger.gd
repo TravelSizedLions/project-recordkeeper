6dd388c651e52f1632f15525063fb6b4c87f7eee
Jeff Hilton <jeffhilton.code@gmail.com>
2023-10-22 00:16:03 -0600

    ephraim re-animated!

--- 0_scripts/sound/music_trigger.gd
+++ 0_scripts/sound/music_trigger.gd
@@ -4,11 +4,15 @@ class_name MusicTrigger
 @export var __start_automatically: bool = true
 
 @export var __music: AudioStream
+@export var __cross_fade: float = 0
+@export var __volume: float = 1
 
 func _ready():
 	if __start_automatically and __music:
-		SoundManager.play_music(__music)
+		SoundManager.set_music_volume(__volume)
+		SoundManager.play_music_at_volume(__music, __volume, __cross_fade)
 
-func _on_trigger():
+func _on_trigger() :
 	if __music:
-		SoundManager.play_music(__music)
+		SoundManager.set_music_volume(__volume)
+		SoundManager.play_music_at_volume(__music, __volume, __cross_fade)
