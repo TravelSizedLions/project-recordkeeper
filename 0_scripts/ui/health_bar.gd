66ccd34ee9801801a38431ff39519cda50fb79b2
Jeff Hilton <jeffhilton.code@gmail.com>
2023-10-28 12:36:36 -0600

    parallax, fix some issues with dying, better health feedback

--- 0_scripts/ui/health_bar.gd
+++ 0_scripts/ui/health_bar.gd
@@ -1,6 +1,10 @@
 extends	ProgressBar
 class_name HealthBar
 
+@export var normal_color: Color = Color.WHITE
+@export var half_health: Color = Color.ORANGE
+@export var danger_color: Color = Color.RED
+
 func _ready():
 	show_percentage = false
 	rounded = true
@@ -13,6 +17,18 @@ func __on_connect_to_entity(_entity):
 
 func _update_health(v: float):
 	value = v
+	if value == 1:
+		self.modulate = danger_color
+	elif value/max_value < 0.5:
+		self.modulate = half_health
+	else:
+		self.modulate = normal_color
 
 func _update_max_health(v: float):
 	max_value = v
+	if value == 1:
+		self.modulate = danger_color
+	elif value/max_value < 0.5:
+		self.modulate = half_health
+	else:
+		self.modulate = normal_color
